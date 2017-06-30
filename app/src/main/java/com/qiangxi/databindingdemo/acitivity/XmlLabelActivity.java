6977f8609f30bd25744735cc72c30ed074285d18
任强强 <981787371@qq.com>
2017-06-30 10:01:09 +0800

    添加截图

---
+++ app/src/main/java/com/qiangxi/databindingdemo/acitivity/XmlLabelActivity.java
@@ -0,0 +1,27 @@
+package com.qiangxi.databindingdemo.acitivity;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qiangxi.databindingdemo.R;
+import com.qiangxi.databindingdemo.databinding.ActivityXmlLabelBinding;
+
+/**
+ * xml标签详解
+ * layout:
+ * data:class(可选)
+ * variable:type(必填)/name(必填)
+ * import:type(必填)/alias(可选)
+ */
+public class XmlLabelActivity extends AppCompatActivity {
+    private ActivityXmlLabelBinding mBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_xml_label);
+        mBinding.setPresenter(this);
+
+    }
+}
